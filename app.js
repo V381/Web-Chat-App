d3a36dc00e6010309999ef4d0058d4de7a44ddcd
Pavle Paunovic <paavlee@yahoo.com>
2023-06-21 21:52:47 +0200

    Nickname implementation

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
+const nicknameSavingHelperFunctions = require("./helpers/read-to-file.js");
+
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const path = require("path");
-const mime = require("mime");
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
-app.post("/set-nickname", (req, res) => {
-    res.send(`New nickname is${re}`);
-});
+
+
+app.post("/set-nickname", async (req, res) => {
+    await nicknameSavingHelperFunctions.readFile(__dirname + "/models/nicknames.json");
+    await nicknameSavingHelperFunctions.updateFile(__dirname + "/models/nicknames.json", req.body.nickname);
+    res.send(`New nickname is${res} ${req.body}`);
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', (msg) => {
         io.emit("user disconected", msg)
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', (msg, nick) => {
+        io.emit('chat message', {message: msg, nickname: nick});
     });
 });
 
